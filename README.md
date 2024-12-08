398967024670c28411298cb9f7380bc0516e9452
Robert Takacs <RoTak@protonmail.com>
2024-12-08 10:22:26 +0200

    Actualizare readme. Adaugiri cod pentru studiu nivel redus. Batch-uri

--- README.md
+++ README.md
@@ -10,6 +10,55 @@ Script-ul python face regresie liniara si compara rezultatele colectate cu o lin
 
 Se observă că datele legate de numărul de semnături sunt, de fapt, incrementate periodic și exact, și deci nu corespund cu un număr adevărat de semnături strânse
 
+## HOW TO USE
+
+### Requirements
+
+- Python 3.x
+- Pandas, Matplotlib, Scipy
+
+```
+pip install pandas matplotlib scipy
+```
+
+### Rulare
+
+```
+python3 test.py [fisier] [nr_esantioane] [offset_esantioane]
+```
+
+#### Argumente
+
+1. [fisier] - by default, fisierul testat va fi output.csv, dar acesta contine multe date preluate la intervale diferite de timp. Recomand rularea cu fisierele batch_1.csv (20:26 - 23:54), batch_2.csv (00:37 - 01:03) sau batch_3.csv (09:59 - 10:13).
+2. [nr_esantioane] - by default, None, deci se folosesc toate datele din fisier. Se poate folosi o valoare cum ar fi 500 pentru a se observa cresterea clar liniara pe intervale mai scurte de timp
+3. [offset_esantioane] - by default, None, deci se incepe de la 0. Se poate folosi in combinatie cu nr_esantioane pentru a studia cresterea pe diferite intervale de timp a nr. de semanturi
+
+### Exemple rulare
+
+`python3 test.py batch_1.csv 500`
+
+![500 de esantioane din primul batch](imagini/fig1.png)
+
+`python3 test.py batch_2.csv 500`
+
+![500 de esantioane din al doilea batch](imagini/fig2.png)
+
+`python3 test.py batch_3.csv`
+
+![Esantioanele din al treilea batch](imagini/fig3.png)
+
+`python3 test.py batch_3.csv`
+
+![Esantioanele din al treilea batch](imagini/fig3.png)
+
+`python3 test.py batch_1.csv 1000 1000`
+
+![1000 de Esantioane, offset cu 1000, din primul batch](imagini/fig4.png)
+
 ## UPDATE
 
 Server-ul da raspuns foarte foarte incet, pot colecta date doar la ~10 secunde. Reparat script-ul pentru a avea in vedere diferentele de timp. E o linie dreapta 100% din nou.
+
+## UPDATE
+
+Site-ul a fost inchis o perioada incepand cu ora 01:00 08.12.2024, si redeschis dimineata. Colectarea datelor a reinceput la ora 09:59
--- test.py
+++ test.py
@@ -1,9 +1,39 @@
 import pandas as pd
 import matplotlib.pyplot as plt
+import sys
+import os
 from scipy.stats import linregress
 
+filename = "output.csv"
+max_samples = None
+sample_offset = None
+
+#get args 
+if len(sys.argv) > 1:
+    filename = sys.argv[1]
+
+if len(sys.argv) > 2:
+    max_samples = int(sys.argv[2])
+
+if len(sys.argv) > 3:
+    sample_offset = int(sys.argv[3])
+
+
+
+#check the file actually exists 
+
+if not os.path.exists(filename):
+    print(f"File {filename} does not exist")
+    sys.exit(1)
+
+if max_samples is not None and max_samples <= 0:
+    print("Max samples must be a positive integer") 
+
+if sample_offset is not None and sample_offset < 0:
+    print("Sample offset must be a non-negative integer")
+
 # Load the CSV file
-data = pd.read_csv("output.csv", names=["Timestamp", "Number"])
+data = pd.read_csv(filename, names=["Timestamp", "Number"])
 
 # Convert the "Timestamp" column to datetime
 data["Timestamp"] = pd.to_datetime(data["Timestamp"], errors="coerce")
@@ -15,6 +45,16 @@ data.dropna(subset=["Timestamp", "Number"], inplace=True)
 # Calculate elapsed time in seconds from the first timestamp
 data["Elapsed_Time"] = (data["Timestamp"] - data["Timestamp"].iloc[0]).dt.total_seconds()
 
+no_values = data["Number"].count()
+
+# if max_rows is set, take the first max_rows rows with offset 
+if max_samples is not None: 
+    if sample_offset is not None:
+        data = data.iloc[sample_offset:sample_offset+max_samples]
+    else:
+        data = data.head(max_samples)
+
+no_reduced_values = data["Number"].count()
 # Perform linear regression using elapsed time
 slope, intercept, r_value, p_value, std_err = linregress(data["Elapsed_Time"], data["Number"])
 
@@ -23,9 +63,9 @@ plt.figure(figsize=(10, 6))
 plt.plot(data["Elapsed_Time"], data["Number"], label="Actual Numbers", marker='o')
 plt.plot(data["Elapsed_Time"], intercept + slope * data["Elapsed_Time"],
          label=f"Linear Fit: y={slope:.2f}x+{intercept:.2f}", color="red")
-plt.title("Number Growth Analysis (Time-Based)")
+plt.title(f"From {data['Timestamp'].iloc[0]} to {data['Timestamp'].iloc[-1]} ({no_reduced_values}/{no_values} values)")
 plt.xlabel("Elapsed Time (seconds)")
-plt.ylabel("Number")
+plt.ylabel("Signatures")
 plt.legend()
 plt.grid()
 plt.show()
